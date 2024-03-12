aac20b540c763b7394a15b852a1372aad1d0fd89
Augusto Lopes <meleu.dev@gmail.com>
2024-03-11 22:05:56 -0300

    add some "mandatory" plugins

---
+++ lua/meleu/plugins/init.lua
@@ -0,0 +1,20 @@
+-- plugins that don't require configurations
+return {
+  -- `gr<text-object>` to replace with the contents of the register
+  "vim-scripts/ReplaceWithRegister",
+
+  -- quickly handling surrounds
+  "tpope/vim-surround",
+
+  -- make vim-surround dot-repeatable
+  "tpope/vim-repeat",
+
+  -- C-a/C-x to increase/decrease dates
+  "tpope/vim-speeddating",
+
+  -- Use `opts = {}` to force a plugin to be loaded.
+  -- This is equivalent to: `require('Comment').setup({})`
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
