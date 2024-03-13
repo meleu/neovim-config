15a8e91004c69ae4a682a33dfc327de6c4156234
Augusto Lopes <meleu.dev@gmail.com>
2024-03-12 23:12:40 -0300

    change telescope keymaps (replace [S]earch with [F]ind)

--- lua/meleu/plugins/which-key.lua
+++ lua/meleu/plugins/which-key.lua
@@ -10,9 +10,8 @@ return {
       ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
       ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
       ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+      ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
       ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
     })
   end,
 }
-
