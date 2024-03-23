e790462c9ecc66e7e0780b97d63f0f3b81bbdc53
meleu <8508804+meleu@users.noreply.github.com>
2024-03-23 20:33:00 -0300

    add borders to completion suggestions

--- lua/meleu/plugins/autocompletion.lua
+++ lua/meleu/plugins/autocompletion.lua
@@ -42,6 +42,10 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       completion = { completeopt = "menu,menuone,noinsert" },
 
       -- For an understanding of why these mappings were
@@ -49,15 +53,15 @@ return {
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
         ["<C-p>"] = cmp.mapping.select_prev_item(),
 
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        -- use <CR> if you want to accept a suggestion with Enter
+        -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
         -- Manually trigger a completion from nvim-cmp.
         --  Generally you don't need this, because nvim-cmp will display
